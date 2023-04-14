3a9fa045a8af7c8767f5124d9c65a661a587772d
Imran Khan <99397606+imran120198@users.noreply.github.com>
2023-04-14 14:08:24 +0530

    feature:: Routes added

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Cycling from "../Pages/Cycling";
+import Featured from "../Pages/Featured";
+import Armoury from "../Pages/Armoury";
+import Aquatics from "../Pages/Aquatics";
+import Equestrain from "../Pages/Equestrain";
+import Extreme from "../Pages/Extreme";
+import Golf from "../Pages/Golf";
+import Gymnastics from "../Pages/Gymnastics";
+import LakeAndSea from "../Pages/LakeAndSea";
 
 const AllRoutes = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        
+        <Route path="/" element={<Cycling />} />
+        <Route path="/featured" element={<Featured />} />
+        <Route path="/armoury" element={<Armoury />} />
+        <Route path="/aquatics" element={<Aquatics />} />
+        <Route path="/equestrian" element={<Equestrain />} />
+        <Route path="/extreme" element={<Extreme />} />
+        <Route path="/golf" element={<Golf />} />
+        <Route path="/gymnastics" element={<Gymnastics />} />
+        <Route path="/lakeandSea" element={<LakeAndSea />} />
       </Routes>
     </div>
   );
